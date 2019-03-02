40a43d633160e5ccb322a0a20053e2d2d2b7d020
ariebrainware <publicu001@gmail.com>
2019-03-02 17:41:24 +0700

    Add react skelton

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,96 +1,307 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true,
-    mocha: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:css-modules/recommended',
-    'airbnb',
-  ],
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2018,
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-      jsx: true,
-      classes: true,
+    'env': {
+        'browser': true,
+        'es6': true
     },
-    sourceType: 'module'
-  },
-  globals: {
-    'google': true,
-    '__VERSION__': true,
-    '__REGION__': true,
-    '__ENV__': true,
-    '__MY__': true,
-    '__SG__': true,
-    '__HK__': true,
-    '__VN__': true,
-    '__PROD__': true,
-    'ga': true,
-    'paypal': true,
-    '__BUILD_TARGET__': true,
-    '$': true,
-    '__DEV__': true,
-  },
-  plugins: ['react', 'import', 'css-modules', 'json'],
-  rules: {
-    indent: ['error', 2],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    camelcase: 0,
-    'max-len': 0,
-    'linebreak-style': ['error', 'unix'],
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true, optionalDependencies: false, peerDependencies: false }],
-    'no-param-reassign': ['error', { props: false }],
-    'object-curly-newline': [0],
-    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    'lines-between-class-members': ['error', 'always'],
-    'arrow-body-style': 0,
-    'no-underscore-dangle': 0,
-    'no-nested-ternary': 1, // warning for now, merchant-portal used alot and related to logic
-
-    // import
-    'import/no-unresolved': [2, { ignore: ['config/message$'] }],
-    'import/prefer-default-export': 0,
-
-    // react
-    'react/forbid-prop-types': [2, { forbid: ['any'] }],
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/no-array-index-key': 1, // warning for now, don't know how to fix
-    "jsx-a11y/anchor-is-valid": [ "error", {
-      components: [ "Link" ],
-      specialLink: [ "to" ],
-      aspects: [ "noHref", "invalidHref", "preferButton" ]
-    }],
-    'jsx-a11y/no-static-element-interactions': 1,
-    'jsx-a11y/click-events-have-key-events': 1,
-
-    // css
-    'css-modules/no-unused-class': 1,
-  },
-  overrides: [
-    {
-      files: ['**/sagas/*.js'],
-      rules: {
-        'no-restricted-syntax': ['error', 'FunctionExpression', 'WithStatement'],
-      },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
     },
-  ],
-  settings: {
-    "import/resolver": {
-      "node": {
-        "extensions": [
-          ".js",
-          ".android.js",
-          ".ios.js"
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'plugins': ['react'],
+    'rules': {
+        'accessor-pairs': 'error',
+        'array-bracket-newline': 'error',
+        'array-bracket-spacing': 'error',
+        'array-callback-return': 'error',
+        'array-element-newline': 'error',
+        'arrow-body-style': 'error',
+        'arrow-parens': [
+            'error',
+            'as-needed'
+        ],
+        'arrow-spacing': [
+            'error',
+            {
+                'after': true,
+                'before': true
+            }
+        ],
+        'block-scoped-var': 'error',
+        'block-spacing': 'error',
+        'brace-style': [
+            'error',
+            '1tbs'
+        ],
+        'callback-return': 'error',
+        'camelcase': 'error',
+        'capitalized-comments': 'off',
+        'class-methods-use-this': 'off',
+        'comma-dangle': 'error',
+        'comma-spacing': [
+            'error',
+            {
+                'after': true,
+                'before': false
+            }
+        ],
+        'comma-style': 'error',
+        'complexity': 'error',
+        'computed-property-spacing': 'error',
+        'consistent-return': 'error',
+        'consistent-this': 'error',
+        'curly': 'error',
+        'default-case': 'error',
+        'dot-location': [
+            'error',
+            'property'
+        ],
+        'dot-notation': [
+            'error',
+            {
+                'allowKeywords': true
+            }
+        ],
+        'eol-last': 'error',
+        'eqeqeq': 'off',
+        'func-call-spacing': 'error',
+        'func-name-matching': 'error',
+        'func-names': 'error',
+        'func-style': [
+            'error',
+            'declaration'
+        ],
+        'function-paren-newline': 'off',
+        'generator-star-spacing': 'error',
+        'global-require': 'error',
+        'guard-for-in': 'error',
+        'handle-callback-err': 'error',
+        'id-blacklist': 'error',
+        'id-length': 'error',
+        'id-match': 'error',
+        'implicit-arrow-linebreak': [
+            'error',
+            'beside'
+        ],
+        'indent': 'off',
+        'indent-legacy': 'off',
+        'init-declarations': 'error',
+        'jsx-quotes': [
+            'error',
+            'prefer-double'
+        ],
+        'key-spacing': 'error',
+        'keyword-spacing': [
+            'error',
+            {
+                'after': true,
+                'before': true
+            }
+        ],
+        'line-comment-position': 'error',
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'lines-around-comment': 'error',
+        'lines-around-directive': 'error',
+        'lines-between-class-members': 'error',
+        'max-classes-per-file': 'error',
+        'max-depth': 'error',
+        'max-len': 'off',
+        'max-lines': 'error',
+        'max-lines-per-function': 'error',
+        'max-nested-callbacks': 'error',
+        'max-params': 'error',
+        'max-statements': 'error',
+        'max-statements-per-line': 'error',
+        'multiline-comment-style': [
+            'error',
+            'separate-lines'
+        ],
+        'multiline-ternary': 'error',
+        'new-cap': 'error',
+        'new-parens': 'error',
+        'newline-after-var': 'off',
+        'newline-before-return': 'error',
+        'newline-per-chained-call': 'error',
+        'no-alert': 'error',
+        'no-array-constructor': 'error',
+        'no-async-promise-executor': 'error',
+        'no-await-in-loop': 'error',
+        'no-bitwise': 'error',
+        'no-buffer-constructor': 'error',
+        'no-caller': 'error',
+        'no-catch-shadow': 'error',
+        'no-confusing-arrow': 'error',
+        'no-continue': 'error',
+        'no-div-regex': 'error',
+        'no-duplicate-imports': 'error',
+        'no-else-return': 'error',
+        'no-empty-function': 'error',
+        'no-eq-null': 'off',
+        'no-eval': 'error',
+        'no-extend-native': 'error',
+        'no-extra-bind': 'error',
+        'no-extra-label': 'error',
+        'no-extra-parens': 'off',
+        'no-floating-decimal': 'error',
+        'no-implicit-coercion': 'error',
+        'no-implicit-globals': 'error',
+        'no-implied-eval': 'error',
+        'no-inline-comments': 'error',
+        'no-invalid-this': 'error',
+        'no-iterator': 'error',
+        'no-label-var': 'error',
+        'no-labels': 'error',
+        'no-lone-blocks': 'error',
+        'no-lonely-if': 'error',
+        'no-loop-func': 'error',
+        'no-magic-numbers': 'off',
+        'no-misleading-character-class': 'error',
+        'no-mixed-operators': 'error',
+        'no-mixed-requires': 'error',
+        'no-multi-assign': 'error',
+        'no-multi-spaces': 'error',
+        'no-multi-str': 'error',
+        'no-multiple-empty-lines': 'error',
+        'no-native-reassign': 'error',
+        'no-negated-condition': 'error',
+        'no-negated-in-lhs': 'error',
+        'no-nested-ternary': 'error',
+        'no-new': 'error',
+        'no-new-func': 'error',
+        'no-new-object': 'error',
+        'no-new-require': 'error',
+        'no-new-wrappers': 'error',
+        'no-octal-escape': 'error',
+        'no-param-reassign': 'error',
+        'no-path-concat': 'error',
+        'no-plusplus': 'error',
+        'no-process-env': 'off',
+        'no-process-exit': 'error',
+        'no-proto': 'error',
+        'no-prototype-builtins': 'error',
+        'no-restricted-globals': 'error',
+        'no-restricted-imports': 'error',
+        'no-restricted-modules': 'error',
+        'no-restricted-properties': 'error',
+        'no-restricted-syntax': 'error',
+        'no-return-assign': 'error',
+        'no-return-await': 'error',
+        'no-script-url': 'error',
+        'no-self-compare': 'error',
+        'no-sequences': 'error',
+        'no-shadow': 'error',
+        'no-shadow-restricted-names': 'error',
+        'no-spaced-func': 'error',
+        'no-sync': 'error',
+        'no-tabs': 'error',
+        'no-template-curly-in-string': 'error',
+        'no-ternary': 'error',
+        'no-throw-literal': 'error',
+        'no-trailing-spaces': 'error',
+        'no-undef-init': 'error',
+        'no-undefined': 'error',
+        'no-underscore-dangle': 'error',
+        'no-unmodified-loop-condition': 'error',
+        'no-unneeded-ternary': 'error',
+        'no-unused-expressions': 'error',
+        'no-use-before-define': 'off',
+        'no-useless-call': 'error',
+        'no-useless-catch': 'error',
+        'no-useless-computed-key': 'error',
+        'no-useless-concat': 'error',
+        'no-useless-constructor': 'error',
+        'no-useless-rename': 'error',
+        'no-useless-return': 'error',
+        'no-var': 'error',
+        'no-void': 'error',
+        'no-warning-comments': 'error',
+        'no-whitespace-before-property': 'error',
+        'no-with': 'error',
+        'nonblock-statement-body-position': 'error',
+        'object-curly-newline': 'error',
+        'object-curly-spacing': [
+            'error',
+            'always'
+        ],
+        'object-property-newline': 'error',
+        'object-shorthand': 'error',
+        'one-var': 'error',
+        'one-var-declaration-per-line': 'error',
+        'operator-assignment': 'error',
+        'operator-linebreak': 'error',
+        'padded-blocks': 'off',
+        'padding-line-between-statements': 'error',
+        'prefer-arrow-callback': 'error',
+        'prefer-const': 'error',
+        'prefer-destructuring': 'error',
+        'prefer-numeric-literals': 'error',
+        'prefer-object-spread': 'error',
+        'prefer-promise-reject-errors': 'error',
+        'prefer-reflect': 'error',
+        'prefer-rest-params': 'error',
+        'prefer-spread': 'error',
+        'prefer-template': 'error',
+        'quote-props': 'error',
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'radix': 'error',
+        'require-atomic-updates': 'error',
+        'require-await': 'error',
+        'require-jsdoc': 'off',
+        'require-unicode-regexp': 'off',
+        'rest-spread-spacing': 'error',
+        'semi': 'error',
+        'semi-spacing': 'error',
+        'semi-style': [
+            'error',
+            'last'
+        ],
+        'sort-keys': 'error',
+        'sort-vars': 'error',
+        'space-before-blocks': 'error',
+        'space-before-function-paren': 'off',
+        'space-in-parens': [
+            'error',
+            'never'
+        ],
+        'space-infix-ops': 'error',
+        'space-unary-ops': 'error',
+        'spaced-comment': [
+            'error',
+            'always'
+        ],
+        'strict': 'error',
+        'switch-colon-spacing': 'error',
+        'symbol-description': 'error',
+        'template-curly-spacing': [
+            'error',
+            'never'
+        ],
+        'template-tag-spacing': 'error',
+        'unicode-bom': [
+            'error',
+            'never'
+        ],
+        'valid-jsdoc': 'error',
+        'vars-on-top': 'error',
+        'wrap-iife': 'error',
+        'wrap-regex': 'error',
+        'yield-star-spacing': 'error',
+        'yoda': [
+            'error',
+            'never'
         ]
-      }
     }
-  },
 };
